666017bf71b6a552c00035df1469a3a12eead1a5
Fan Jiang <prof.fan@foxmail.com>
2020-05-02 00:59:29 -0400

    Proper Rot3 Local()

--- Sources/SwiftFusion/Geometry/Rot3.swift
+++ Sources/SwiftFusion/Geometry/Rot3.swift
@@ -1,6 +1,12 @@
 
 import TensorFlow
 
+extension Vector3 {
+  static func * (_ s:Double, _ vector:Vector3) -> Vector3 {
+    Vector3(s * vector.x, s * vector.y, s * vector.z)
+  }
+}
+
 public struct Rot3: Manifold, TangentStandardBasis, Equatable, KeyPathIterable {
   public typealias TangentVector = Vector3
   // MARK: - Manifold conformance
@@ -12,16 +18,22 @@ public struct Rot3: Manifold, TangentStandardBasis, Equatable, KeyPathIterable {
     coordinateStorage = coordinateStorage.global(direction)
   }
 
-// BUG(fan): will trigger ICE
-//  /// Construct from a rotation matrix, as doubles in *row-major* order
-//  @differentiable
-//  public init(_ r11 : Double, _ r12 : Double, _ r13 : Double,
-//              _ r21 : Double, _ r22 : Double, _ r23 : Double,
-//              _ r31 : Double, _ r32 : Double, _ r33 : Double) {
-//    self.init(coordinateStorage: Matrix3Coordinate(r11, r12, r13,
-//              r21, r22, r23,
-//              r31, r32, r33))
-//  }
+  /// Construct from a rotation matrix, as doubles in *row-major* order
+  @differentiable
+  public init(_ r11 : Double, _ r12 : Double, _ r13 : Double,
+              _ r21 : Double, _ r22 : Double, _ r23 : Double,
+              _ r31 : Double, _ r32 : Double, _ r33 : Double) {
+    self.init(coordinate: Matrix3Coordinate(r11, r12, r13,
+              r21, r22, r23,
+              r31, r32, r33))
+  }
+  
+  public static func fromTangent(_ vector: Vector3) -> Self {
+    var actual = Rot3()
+    actual.move(along: vector)
+    return actual
+  }
+  
   public init() {
     self.init(coordinateStorage: Matrix3Coordinate(eye(rowCount: 3)))
   }
@@ -89,7 +101,39 @@ extension Matrix3Coordinate: ManifoldCoordinate {
 
   @differentiable(wrt: global)
   public func local(_ global: Matrix3Coordinate) -> Vector3 {
-    Vector3(1,2,3)
+    let (R11, R12, R13) = (R[0, 0].scalar!, R[0, 1].scalar!, R[0, 2].scalar!)
+    let (R21, R22, R23) = (R[1, 0].scalar!, R[1, 1].scalar!, R[1, 2].scalar!)
+    let (R31, R32, R33) = (R[2, 0].scalar!, R[2, 1].scalar!, R[2, 2].scalar!)
+
+    let tr = R.diagonalPart().sum().scalar!
+
+    var omega: Vector3
+    
+    print("trace = \(tr)")
+    if abs(tr + 1.0) < 1e-10 {
+      if abs(R33 + 1.0) > 1e-10 {
+        omega = (.pi / sqrtWrap(2.0 + 2.0 * R33)) * Vector3(R13, R23, 1.0 + R33)
+      } else if abs(R22 + 1.0) > 1e-10 {
+        omega = (.pi / sqrtWrap(2.0 + 2.0 * R22)) * Vector3(R12, 1.0 + R22, R32)
+      } else {
+        // if(abs(R.r1_.x()+1.0) > 1e-10)  This is implicit
+        omega = (.pi / sqrtWrap(2.0 + 2.0 * R11)) * Vector3(1.0 + R11, R21, R31)
+      }
+    } else {
+      var magnitude: Double
+
+      let tr_3 = tr - 3.0; // always negative
+      if tr_3 < -1e-7 {
+        let theta = acos((tr - 1.0) / 2.0)
+        magnitude = theta / (2.0 * sin(theta))
+      } else {
+        // when theta near 0, +-2pi, +-4pi, etc. (trace near 3.0)
+        // use Taylor expansion: theta \approx 1/2-(t-3)/12 + O((t-3)^2)
+        magnitude = 0.5 - tr_3 * tr_3 / 12.0;
+      }
+      omega = magnitude * Vector3(R32 - R23, R13 - R31, R21 - R12);
+    }
+    return omega
   }
 
   /// Construct from Tensor
--- Tests/SwiftFusionTests/Geometry/Rot3Tests.swift
+++ Tests/SwiftFusionTests/Geometry/Rot3Tests.swift
@@ -10,17 +10,48 @@ final class Rot3Tests: XCTestCase {
   // test constructor from doubles
   func testConstructorEquality() {
     let R1 = Rot3()
-    let R2 = Rot3(coordinate: Matrix3Coordinate(1, 0, 0,  0, 1, 0,  0, 0, 1))
+    let R2 = Rot3(1, 0, 0,  0, 1, 0,  0, 0, 1)
     XCTAssertEqual(R1, R2)
   }
 
   // test constructor from doubles, non-identity
   func testConstructorInequal() {
     let R1 = Rot3()
-    let R2 = Rot3(coordinate: Matrix3Coordinate(0, 1, 0, 1, 0, 0, 0, 0, -1))
+    let R2 = Rot3(0, 1, 0, 1, 0, 0, 0, 0, -1)
     XCTAssertNotEqual(R1, R2)
   }
 
+  /// Tests that the manifold invariant holds for Rot3
+  func testManifoldIdentity() {
+    for _ in 0..<10 {
+      let p = Rot3.fromTangent(Vector3(Tensor<Double>(randomNormal: [3])))
+      let q = Rot3.fromTangent(Vector3(Tensor<Double>(randomNormal: [3])))
+      let actual: Rot3 = Rot3(coordinate: p.coordinate.global(p.coordinate.local(q.coordinate)))
+      assertAllKeyPathEqual(actual, q, accuracy: 1e-10)
+    }
+  }
+  
+  /// Tests that the manifold invariant holds for Rot3
+  /// (-1+2n) * pi
+  func testManifoldIdentitySpecial1() {
+    for i in -5..<5 {
+      let p = Rot3.fromTangent(Vector3(Double(2*i - 1) * .pi, 0, 0))
+      let q = Rot3.fromTangent(Vector3(Tensor<Double>(randomNormal: [3])))
+      let actual: Rot3 = Rot3(coordinate: p.coordinate.global(p.coordinate.local(q.coordinate)))
+      assertAllKeyPathEqual(actual, q, accuracy: 1e-10)
+    }
+  }
+  
+  /// Tests that the manifold invariant holds for Rot3
+  func testManifoldIdentitySpecial2() {
+    for _ in 0..<10 {
+      let p = Rot3.fromTangent(Vector3(1e-10 * Tensor<Double>(randomNormal: [3])))
+      let q = Rot3.fromTangent(Vector3(Tensor<Double>(randomNormal: [3])))
+      let actual: Rot3 = Rot3(coordinate: p.coordinate.global(p.coordinate.local(q.coordinate)))
+      assertAllKeyPathEqual(actual, q, accuracy: 1e-10)
+    }
+  }
+  
   // test that move really works
   func testMove() {
     let xi = Vector3(-.pi/2,0,0)
@@ -45,6 +76,18 @@ final class Rot3Tests: XCTestCase {
     actual.move(along: v)
     
     assertEqual(actual.coordinate.R, expected, accuracy: 1e-5)
+  }
+  
+  func testExpmapNearZero() {
+    let axis = Vector3(0, 1, 0)  // rotation around Y
+    let angle = 0.0
+    let v = axis.scaled(by: angle)
+    let expected = Rot3()
+    
+    var actual = Rot3()
+    actual.move(along: v)
+    
+    assertAllKeyPathEqual(actual, expected, accuracy: 1e-5)
   }
   
   static var allTests = [
