ca529846fa6a5b4e0cc4a3dbaffc87f628e9f8a3
Marc Rasi <marcrasi@google.com>
2020-05-18 18:41:01 -0700

    more work on fast poseslam

--- Sources/Benchmarks/Pose2SLAM.swift
+++ Sources/Benchmarks/Pose2SLAM.swift
@@ -19,8 +19,8 @@ import SwiftFusion
 
 let pose2SLAM = BenchmarkSuite(name: "Pose2SLAM") { suite in
 
-  let intelDataset = try! G2OFactorGraph(fromG2O: try! cachedDataset("input_INTEL_g2o.txt"))
-  check(intelDataset.graph.error(intelDataset.initialGuess), near: 73565.64, accuracy: 1e-2)
+  let intelNonlinearFactorGraph = try! G2OFactorGraph(fromG2O: try! cachedDataset("input_INTEL_g2o.txt"))
+  check(intelNonlinearFactorGraph.graph.error(intelNonlinearFactorGraph.initialGuess), near: 73565.64, accuracy: 1e-2)
 
   // Uses `NonlinearFactorGraph` on the Intel dataset.
   // The solvers are configured to run for a constant number of steps.
@@ -30,19 +30,45 @@ let pose2SLAM = BenchmarkSuite(name: "Pose2SLAM") { suite in
     "NonlinearFactorGraph, Intel, 5 Gauss-Newton steps, 100 CGLS steps",
     settings: .iterations(1)
   ) {
-    var val = intelDataset.initialGuess
+    var val = intelNonlinearFactorGraph.initialGuess
     for _ in 0..<5 {
-      let gfg = intelDataset.graph.linearize(val)
+      let gfg = intelNonlinearFactorGraph.graph.linearize(val)
       let optimizer = CGLS(precision: 0, max_iteration: 100)
       var dx = VectorValues()
       for i in 0..<val.count {
         dx.insert(i, Vector(zeros: 3))
       }
-      optimizer.optimize(gfg: gfg, initial: &dx)
+      optimizer.optimize(gfg, initial: &dx)
       val.move(along: dx)
     }
-    check(intelDataset.graph.error(val), near: 35.59, accuracy: 1e-2)
+    check(intelNonlinearFactorGraph.graph.error(val), near: 20.87, accuracy: 1e-2)
   }
+
+  let intelPoseSLAMFactorGraph = try! G2OPoseSLAMFactorGraph(fromG2O: try! cachedDataset("input_INTEL_g2o.txt"))
+
+  // Uses `PoseSLAMFactorGraph` on the Intel dataset.
+  // The solvers are configured to run for a constant number of steps.
+  // The nonlinear solver is 5 iterations of Gauss-Newton.
+  // The linear solver is 100 iterations of CGLS.
+  suite.benchmark(
+    "PoseSLAMFactorGraph, Intel, 5 Gauss-Newton steps, 100 CGLS steps",
+    settings: .iterations(1)
+  ) {
+    var graph = intelPoseSLAMFactorGraph.graph
+    graph.priors.parameters.append(Pose2(0, 0, 0))
+    graph.priorInputs.append(0)
+    var val = intelPoseSLAMFactorGraph.initialGuess
+    for _ in 0..<5 {
+      let gfg = intelPoseSLAMFactorGraph.graph.linearized(at: val)
+      let optimizer = CGLS(precision: 0, max_iteration: 100)
+      var dx = gfg.zeroInput
+      optimizer.optimize(gfg, initial: &dx)
+      for index in val.indices {
+        val[index].move(along: dx[index])
+      }
+    }
+  }
+
 }
 
 /// Builds an initial guess and a factor graph from a g2o file.
@@ -59,6 +85,25 @@ struct G2OFactorGraph: G2OReader {
 
   public mutating func addMeasurement(frameIndex: Int, measuredIndex: Int, pose: Pose2) {
     graph += BetweenFactor(frameIndex, measuredIndex, pose)
+  }
+}
+
+/// Builds an initial guess and a factor graph from a g2o file.
+struct G2OPoseSLAMFactorGraph: G2OReader {
+  /// The initial guess.
+  var initialGuess: [Pose2] = []
+
+  /// The factor graph representing the measurements.
+  var graph: Pose2SLAMFactorGraph = Pose2SLAMFactorGraph()
+
+  public mutating func addInitialGuess(index: Int, pose: Pose2) {
+    precondition(index == initialGuess.count)
+    initialGuess.append(pose)
+  }
+
+  public mutating func addMeasurement(frameIndex: Int, measuredIndex: Int, pose: Pose2) {
+    graph.betweens.parameters.append(pose)
+    graph.betweenInputs.append((frameIndex, measuredIndex))
   }
 }
 
--- Sources/SwiftFusion/Core/FixedDimensionVector.swift
+++ Sources/SwiftFusion/Core/FixedDimensionVector.swift
@@ -1,4 +1,29 @@
 /// A vector with a compiletime fixed dimension.
 public protocol FixedDimensionVector {
   static var dimension: Int { get }
+
+  subscript(_ index: Int) -> Double { get set }
+
+  static var zero: Self { get }
+}
+
+extension FixedDimensionVector {
+  public init<C: Collection>(_ scalars: C) where C.Element == Double {
+    self = Self.zero
+    for (index, scalar) in zip(0..<(Self.dimension), scalars) {
+      self[index] = scalar
+    }
+  }
+
+  public var scalars: LazyMapCollection<Range<Int>, Double> {
+    return (0..<(Self.dimension)).lazy.map { self[$0] }
+  }
+
+  public static var standardBasis: LazyMapCollection<Range<Int>, Self> {
+    return (0..<(Self.dimension)).lazy.map { index in
+      var vector = Self.zero
+      vector[index] = 1
+      return vector
+    }
+  }
 }
