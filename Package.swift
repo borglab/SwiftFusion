1c51afcd91b6f7f54ffc302a16874e69d3c558b3
marcrasi <marcrasi@google.com>
2020-06-11 22:19:49 -0700

    switch to pengiun master

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/google/swift-benchmark.git", .branch("master")),
-    .package(url: "https://github.com/saeta/penguin.git", .branch("marcrasi/arraybuffer_cast")),
+    .package(url: "https://github.com/saeta/penguin.git", .branch("master")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
