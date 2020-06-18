e261e11e695ead9f071f0de809f165d216ba081f
Fan Jiang <i@fanjiang.me>
2020-06-17 23:03:14 -0400

    Latest deps

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/google/swift-benchmark.git", .branch("master")),
     .package(url: "https://github.com/saeta/penguin.git", .branch("master")),
-    .package(url: "https://github.com/ProfFan/tensorboardx-s4tf.git", .branch("master")),
+    .package(url: "https://github.com/ProfFan/tensorboardx-s4tf.git", from: "0.1.3"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
