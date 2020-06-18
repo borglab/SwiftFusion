469cb6bc061f386ade0ff111026010f790b0b2a4
Frank Dellaert <dellaert@cc.gatech.edu>
2020-06-18 18:31:03 -0400

    attempt at switching model

--- Tests/SwiftFusionTests/Inference/Scratch.swift
+++ Tests/SwiftFusionTests/Inference/Scratch.swift
@@ -5,31 +5,126 @@ import XCTest
 import PenguinStructures
 @testable import SwiftFusion
 
+/// A factor with a switchable motion model.
+///
+/// `JacobianRows` specifies the `Rows` parameter of the Jacobian of this factor. See the
+/// documentation on `NewJacobianFactor.jacobian` for more information. Use the typealiases below to
+/// avoid specifying this type parameter every time you create an instance.
+public struct NewSwitchingBetweenFactor<Pose: LieGroup, JacobianRows: FixedSizeArray>:
+  NewLinearizableFactor
+  where JacobianRows.Element == Tuple2<Pose.TangentVector, Pose.TangentVector>
+{
+  public typealias Variables = Tuple3<Pose, Int, Pose>
+
+  public let edges: Variables.Indices
+  
+  /// Movement temmplates for each label.
+  let movements: [Pose]
+
+  public init(_ from: TypedID<Pose, Int>,
+              _ label: TypedID<Int, Int>,
+              _ to: TypedID<Pose, Int>,
+              _ movements: [Pose]) {
+    self.edges = Tuple3(from, label, to)
+    self.movements = movements
+  }
+
+  public typealias ErrorVector = Pose.TangentVector
+
+  @differentiable(wrt: (start, end))
+  public func errorVector(_ start: Pose, _ label: Int, _ end: Pose) -> ErrorVector {
+    let actualMotion = between(start, end)
+    return movements[label].localCoordinate(actualMotion)
+  }
+
+  // Note: All the remaining code in this factor is boilerplate that we can eventually eliminate
+  // with sugar.
+  
+  public func error(at x: Variables) -> Double {
+    return errorVector(at: x).squaredNorm
+  }
+
+  public func errorVector(at x: Variables) -> Pose.TangentVector {
+    return errorVector(x.head, x.tail.head, x.tail.tail.head)
+  }
+
+  public typealias Linearization = NewJacobianFactor<JacobianRows, ErrorVector>
+  public func linearized(at x: Variables) -> Linearization {
+    Linearization(linearizing: errorVector, at: x, edges: edges)
+  }
+}
+
+/// A between factor on `Pose2`.
+public typealias NewSwitchingBetweenFactor2 = NewSwitchingBetweenFactor<Pose2, Array3<Tuple2<Vector3, Vector3>>>
+
+/// A between factor on `Pose3`.
+public typealias NewSwitchingBetweenFactor3 = NewSwitchingBetweenFactor<Pose3, Array6<Tuple2<Vector6, Vector6>>>
+
 class Scratch: XCTestCase {
   func createTrackingFactorGraph() -> (NewFactorGraph, VariableAssignments) {
     var variables = VariableAssignments()
-    let keys = repeatElement(Pose2(0, 0, 0), count: 3).map { variables.store($0) }
-
-    let measurements = [
-      Pose2(10, 0, 0),
-      Pose2(11, 0, 0),
-      Pose2(12, 0, 0)
-    ]
+    let x1 = variables.store(Pose2(0, 0, 0))
+    let x2 = variables.store(Pose2(0, 0, 0))
+    let x3 = variables.store(Pose2(0, 0, 0))
 
     var graph = NewFactorGraph()
-    graph.store(NewPriorFactor2(keys[0], Pose2(10, 0, 0)))
-    for i in 0..<3 {
-      graph.store(NewPriorFactor2(keys[i], measurements[i]))
-    }
-    graph.store(NewBetweenFactor2(keys[0], keys[1], Pose2(1, 0, 0)))
-    graph.store(NewBetweenFactor2(keys[1], keys[2], Pose2(1, 0, 0)))
+    graph.store(NewPriorFactor2(x1, Pose2(10, 0, 0)))
+    graph.store(NewPriorFactor2(x1, Pose2(10, 0, 0)))
+    graph.store(NewPriorFactor2(x2, Pose2(11, 0, 0)))
+    graph.store(NewPriorFactor2(x3, Pose2(12, 0, 0)))
+    graph.store(NewBetweenFactor2(x1, x2, Pose2(1, 0, 0)))
+    graph.store(NewBetweenFactor2(x2, x3, Pose2(1, 0, 0)))
 
     return (graph, variables)
   }
 
-  func testWholeEnchilada() {
+  /// Tracking example from Figure 2.a
+  func testTrackingExample() {
     // create a factor graph
     var (graph, variables) = createTrackingFactorGraph()
+    _ = graph as NewFactorGraph
+
+    XCTAssertEqual(graph.storage.count, 2)
+
+    // optimize
+    var opt = LM()
+    try! opt.optimize(graph: graph, initial: &variables)
+
+    // check results
+    print(variables[TypedID<Pose2, Int>(0)])
+    print(variables[TypedID<Pose2, Int>(1)])
+    print(variables[TypedID<Pose2, Int>(2)])
+  }
+
+  func createSwitchingFactorGraph() -> (NewFactorGraph, VariableAssignments) {
+    var variables = VariableAssignments()
+    let x1 = variables.store(Pose2(0, 0, 0))
+    let x2 = variables.store(Pose2(0, 0, 0))
+    let x3 = variables.store(Pose2(0, 0, 0))
+    let q1 = variables.store(0)
+    let q2 = variables.store(0)
+
+    let movements = [
+      Pose2(1, 0, 0),       // go forwards
+      Pose2(1, 0, .pi / 4), // turn left
+      Pose2(1, 0, -.pi / 4)  // turn right
+    ]
+
+    var graph = NewFactorGraph()
+    graph.store(NewPriorFactor2(x1, Pose2(10, 0, 0)))
+    graph.store(NewPriorFactor2(x1, Pose2(10, 0, 0)))
+    graph.store(NewPriorFactor2(x2, Pose2(11, 0, 0)))
+    graph.store(NewPriorFactor2(x3, Pose2(12, 0, 0)))
+    graph.store(NewSwitchingBetweenFactor2(x1, q1, x2, movements))
+    graph.store(NewSwitchingBetweenFactor2(x2, q2, x3, movements))
+
+    return (graph, variables)
+  }
+
+  /// Tracking switching from Figure 2.b
+  func testSwitchingExample() {
+    // create a factor graph
+    var (graph, variables) = createSwitchingFactorGraph()
     _ = graph as NewFactorGraph
 
     XCTAssertEqual(graph.storage.count, 2)
