668830d6950ed86bf19fc67b3059a2a0293b90cf
Dave Abrahams <dabrahams@google.com>
2020-07-16 18:09:55 -0700

    Extension has moved to penguin.

--- Sources/SwiftFusion/Inference/PenguinExtensions.swift
+++ Sources/SwiftFusion/Inference/PenguinExtensions.swift
@@ -11,31 +11,6 @@ extension AnyArrayBuffer {
   }
 }
 
-/// A type-erased array that is not statically known to support any operations.
-typealias AnyElementArrayBuffer = AnyArrayBuffer<AnyObject>
-
-extension ArrayBuffer {
-  /// Creates an instance with the given `count`, and capacity at least
-  /// `minimumCapacity`, and elements initialized by `initializeElements`,
-  /// which is passed the address of the (uninitialized) first element.
-  ///
-  /// - Requires: `initializeElements` initializes exactly `count` contiguous
-  ///   elements starting with the address it is passed.
-  public init(
-    count: Int,
-    minimumCapacity: Int = 0,
-    initializeElements:
-      (_ uninitializedElements: UnsafeMutablePointer<Element>) -> Void
-  ) {
-    // Work around the fact that `ArrayBuffer.init(storage:)` doesn't exist. We can just set
-    // `storage` directly without calling a different initializer first when we move this
-    // implementation to penguin.
-    self.init(minimumCapacity: 0)
-    self.storage = .init(
-      count: count, minimumCapacity: minimumCapacity, initializeElements: initializeElements)
-  }
-}
-
 extension UnsafeRawPointer {
   /// Returns the `T` to which `self` points.
   internal subscript<T>(as _: Type<T>) -> T {
