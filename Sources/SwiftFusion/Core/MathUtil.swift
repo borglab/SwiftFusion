914a8364e81b3bc4446afc12c7665c21513964ff
Fan Jiang <i@fanjiang.me>
2020-07-28 21:55:16 +0000

    W0rking PPCA reconstruction

--- Sources/SwiftFusion/Core/MathUtil.swift
+++ Sources/SwiftFusion/Core/MathUtil.swift
@@ -4,16 +4,16 @@
 
 import TensorFlow
 
-public func pinv(_ m: Tensor<Double>) -> Tensor<Double> {
+public func pinv<Scalar: TensorFlowFloatingPoint>(_ m: Tensor<Scalar>) -> Tensor<Scalar> {
   let (J_s, J_u, J_v) = m.svd(computeUV: true, fullMatrices: true)
   
   let m = J_v!.shape[1]
   let n = J_u!.shape[0]
   if (m > n) {
-    let J_ss = J_s.reciprocal.diagonal().concatenated(with: Tensor<Double>(repeating: 0, shape: [m-n, n]), alongAxis: 0)
+    let J_ss = J_s.reciprocal.diagonal().concatenated(with: Tensor<Scalar>(repeating: 0, shape: [m-n, n]), alongAxis: 0)
     return matmul(matmul(J_v!, J_ss), J_u!.transposed())
   } else if (m < n) {
-    let J_ss = J_s.reciprocal.diagonal().concatenated(with: Tensor<Double>(repeating: 0, shape: [m, n-m]), alongAxis: 1)
+    let J_ss = J_s.reciprocal.diagonal().concatenated(with: Tensor<Scalar>(repeating: 0, shape: [m, n-m]), alongAxis: 1)
     return matmul(matmul(J_v!, J_ss), J_u!.transposed())
   } else {
     let J_ss = J_s.reciprocal.diagonal()
