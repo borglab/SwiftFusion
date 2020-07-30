e3859c0c69833d06bc0135adfb2cb28ecdade973
Dave Abrahams <dabrahams@google.com>
2020-07-30 12:35:57 -0700

    Crash reproducer

--- Sources/SwiftFusion/Inference/ArrayBuffer+Vector.swift
+++ Sources/SwiftFusion/Inference/ArrayBuffer+Vector.swift
@@ -13,8 +13,9 @@
 // limitations under the License.
 import PenguinStructures
 
-extension ArrayBuffer/*: EuclideanVector*/ where Element: EuclideanVector {
+extension ArrayBuffer: EuclideanVector where Element: EuclideanVector {
   /// Replaces lhs with the product of `lhs` and `rhs`.
+  @differentiable
   public static func *= (lhs: inout ArrayBuffer, rhs: Double) -> Void {
     if lhs.isEmpty { return }
     if rhs == 0 { lhs = .zero }
@@ -23,17 +24,41 @@ extension ArrayBuffer/*: EuclideanVector*/ where Element: EuclideanVector {
     }
   }
 
+  @derivative(of: *=)
+  @usableFromInline
+  static func vjp_timesEqual(lhs: inout ArrayBuffer, rhs: Double)
+    -> (value: Void, pullback: (inout TangentVector)->Double)
+  {
+    let oldLHS = lhs
+    lhs *= rhs
+    return ((),
+            { tangent in
+              tangent *= rhs
+              return oldLHS.dot(tangent)
+            })
+  }
+
   /// Returns the product of `lhs` and `rhs`.
-  public static func * (lhs: ArrayBuffer, rhs: Double) -> ArrayBuffer {
-    if lhs.isEmpty { return lhs }
-    if rhs == 0 { return .zero }
-    return .init(lhs.lazy.map { rhs * $0 })
+  @differentiable
+  public static func * (lhs: Double, rhs: ArrayBuffer) -> ArrayBuffer {
+    if rhs.isEmpty { return rhs }
+    if lhs == 0 { return .zero }
+    return .init(rhs.lazy.map { lhs * $0 })
+  }
+
+  @derivative(of: *)
+  @usableFromInline
+  static func vjp_timesEqual(lhs: Double, rhs: ArrayBuffer)
+    -> (value: ArrayBuffer, pullback: (TangentVector)->(Double, TangentVector))
+  {
+    return (lhs * rhs, { tangent in (rhs.dot(tangent), lhs * tangent) })
   }
 
   /// Returns the dot product of `self` with `other`.
   ///
   /// - Requires: `self.tensorShapeIsCompatible(withThatOf: other)`
-  public func dot(other: ArrayBuffer) -> Double {
+  @differentiable
+  public func dot(_ other: ArrayBuffer) -> Double {
     if self.isEmpty || other.isEmpty { return 0 }
     assert(self.tensorShapeIsCompatible(withThatOf: other))
     return self.withUnsafeBufferPointer { lhs in
@@ -41,6 +66,14 @@ extension ArrayBuffer/*: EuclideanVector*/ where Element: EuclideanVector {
         (0..<lhs.count).reduce(0) { sum, i in sum + lhs[i].dot(rhs[i]) }
       }
     }
+  }
+
+  @derivative(of: dot)
+  @usableFromInline
+  func vjp_dot(_ other: ArrayBuffer)
+    -> (value: Double, pullback: (Double)->(TangentVector, TangentVector))
+  {
+    return (self.dot(other), { r in (r * other, r * self) })
   }
 
   /// Creates an instance whose elements are `scalars`.
