99dea8657bdad69f7bab54a43b93a6cd9220ba42
Fan Jiang <i@fanjiang.me>
2020-08-04 20:40:49 +0000

    Add comments on PPCA

--- Tests/SwiftFusionTests/Inference/PPCATests.swift
+++ Tests/SwiftFusionTests/Inference/PPCATests.swift
@@ -19,46 +19,52 @@ import XCTest
 import PenguinStructures
 import SwiftFusion
 
+/// This class tests the PPCA (Probablistic PCA) algorithm.
 class PPCATests: XCTestCase {
+
+  /// Tests simple forward pass with zero error
   func testSimplePPCAForwardZero() {
     var x = VariableAssignments()
-    let a = x.store(Vector5(1, 1, 1, 1, 1))
+    let latentKey = x.store(Vector5(1, 1, 1, 1, 1))
     let imagePatch = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
     let mu = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
-    let W: Tensor<Double> = Tensor<Double>(zeros: [10, 10, 5])
+    let W = Tensor<Double>(zeros: [10, 10, 5])
     var graph = FactorGraph()
-    graph.store(PPCAFactor(a, desired: imagePatch, W: W, mu: mu))
-    graph.store(PriorFactor<Vector5>(a, Vector5(1, 1, 1, 1, 1)))
+    graph.store(PPCAFactor(latentKey, measured: imagePatch, W: W, mu: mu))
+    graph.store(PriorFactor<Vector5>(latentKey, Vector5(1, 1, 1, 1, 1)))
 
     XCTAssertEqual(graph.error(at: x), 0.0)
   }
 
+  /// Tests a trivial forward pass with 0.5 * 1^2 error
   func testSimplePPCAForwardSimple() {
     var x = VariableAssignments()
-    let a = x.store(Vector5(1, 1, 1, 1, 1))
+    let latentKey = x.store(Vector5(1, 1, 1, 1, 1))
     let imagePatch = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
     let mu = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
-    var W: Tensor<Double> = Tensor<Double>(zeros: [10, 10, 5])
+    var W = Tensor<Double>(zeros: [10, 10, 5])
     W[0, 0, 0] = Tensor(1.0)
     var graph = FactorGraph()
-    graph.store(PPCAFactor(a, desired: imagePatch, W: W, mu: mu))
-    graph.store(PriorFactor<Vector5>(a, Vector5(1, 1, 1, 1, 1)))
+    graph.store(PPCAFactor(latentKey, measured: imagePatch, W: W, mu: mu))
+    graph.store(PriorFactor<Vector5>(latentKey, Vector5(1, 1, 1, 1, 1)))
 
+    // Only W[0,0,0] is 1, so cost is 0.5 * 1^2 = 0.5
     XCTAssertEqual(graph.error(at: x), 0.5)
   }
 
+  /// Tests whether the factor is properly linearized with correct derivatives
   func testSimplePPCALinearization() {
     let imagePatch = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
     let mu = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
 
-    var W: Tensor<Double> = Tensor<Double>(zeros: [10, 10, 5])
+    var W = Tensor<Double>(zeros: [10, 10, 5])
     W[0, 0, 0] = Tensor(1.0)
     W[0, 0, 1] = Tensor(1.0)
     W[0, 0, 2] = Tensor(1.0)
     W[0, 0, 3] = Tensor(1.0)
     W[0, 0, 4] = Tensor(1.0)
 
-    let ppcaFactor = PPCAFactor(TypedID(0), desired: imagePatch, W: W, mu: mu)
+    let ppcaFactor = PPCAFactor(TypedID(0), measured: imagePatch, W: W, mu: mu)
     let jf = JacobianFactor100x5_1(linearizing: ppcaFactor, at: Tuple1(Vector5(1, 1, 1, 1, 1)))
     let jacobianPPCA = Tensor<Double>(stacking: jf.jacobian.map { $0.flatTensor })
 
@@ -72,9 +78,10 @@ class PPCATests: XCTestCase {
     assertEqual(jacobianPPCA, expectedJacobianPPCA, accuracy: 1e-10)
   }
 
+  /// Tests solving a factor graph with PPCA factors
   func testSimplePPCASolve() {
     var x = VariableAssignments()
-    let a = x.store(Vector5(1, 1, 1, 1, 1))
+    let latentKey = x.store(Vector5(1, 1, 1, 1, 1))
 
     /// imagePatch will only have one pixel with value 1
     var imagePatch = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
@@ -83,7 +90,7 @@ class PPCATests: XCTestCase {
     let mu = Tensor10x10(Tensor<Double>(zeros: [10, 10]))
 
     /// Weight only applies to first pixel
-    var W: Tensor<Double> = Tensor<Double>(zeros: [10, 10, 5])
+    var W = Tensor<Double>(zeros: [10, 10, 5])
     W[0, 0, 0] = Tensor(1.0)
     W[0, 0, 1] = Tensor(-1.0)
     W[0, 0, 2] = Tensor(0.0)
@@ -91,10 +98,10 @@ class PPCATests: XCTestCase {
     W[0, 0, 4] = Tensor(-1.0)
 
     var graph = FactorGraph()
-    graph.store(PPCAFactor(a, desired: imagePatch, W: W, mu: mu))
-    graph.store(PriorFactor<Vector5>(a, Vector5(1, 1, 1, 1, 1)))
+    graph.store(PPCAFactor(latentKey, measured: imagePatch, W: W, mu: mu))
+    graph.store(PriorFactor<Vector5>(latentKey, Vector5(1, 1, 1, 1, 1)))
 
-    for _ in 0..<2 {
+    for _ in 0..<1 {
       let gfg = graph.linearized(at: x)
       var dx = x.tangentVectorZeros
       var optimizer = GenericCGLS(precision: 1e-6, max_iteration: 500)
@@ -102,6 +109,6 @@ class PPCATests: XCTestCase {
       x.move(along: dx)
     }
 
-    assertAllKeyPathEqual(x[a], Vector5(1.2, 0.8, 1.0, 1.2, 0.8), accuracy: 1e-7)
+    assertAllKeyPathEqual(x[latentKey], Vector5(1.2, 0.8, 1.0, 1.2, 0.8), accuracy: 1e-7)
   }
 }
