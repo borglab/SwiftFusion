d22f932bafd116dcaa96fa16a370af2b9ac81eb0
Marc Rasi <marcrasi@google.com>
2020-08-05 22:21:09 -0700

    faster crop

--- Sources/SwiftFusion/Image/Patch.swift
+++ Sources/SwiftFusion/Image/Patch.swift
@@ -14,39 +14,78 @@
 
 import TensorFlow
 
-extension Tensor where Scalar == Double {
-  /// Returns the patch of `self` at `region`, sampling pixels using `resample`.
+extension ArrayImage {
+  /// Returns the patch of `self` at `region`.
   ///
   /// - Parameters:
-  ///   - self: a pixel tensor with shape `[height, width, channelCount]`. `height` and `width` are
-  ///     as defined in `docs/ImageOperations.md`.
   ///   - region: the center, orientation, and size of the patch to extract.
-  ///   - resample: Returns a sample of `image` at `point`.
-  ///     - image: a pixel tensor, with shape `[height, width, channelCount]`. `height` and
-  ///       `width` are as defined in `docs/ImageOperations.md`.
-  ///     - point: a point in `(u, v)` coordinates as defined in `docs/ImageOperations.md`.
-  @differentiable
-  public func patch(
-    at region: OrientedBoundingBox,
-    resample: @differentiable (_ image: Tensor<Scalar>, _ point: Vector2) -> Tensor<Scalar>
-      = bilinear
-  ) -> Tensor<Scalar> {
-    precondition(self.shape.count == 3, "image must have shape height x width x channelCount")
-    let patchShape: TensorShape = [region.rows, region.cols, self.shape[2]]
-    var patch = Tensor<Scalar>(zeros: patchShape)
+  @differentiable(wrt: region)
+  public func patch(at region: OrientedBoundingBox) -> Self {
+    var result = ArrayImage(rows: region.rows, cols: region.cols, channels: self.channels)
     for i in 0..<region.rows {
       for j in 0..<region.cols {
         // The position of the destination pixel in the destination image, in `(u, v)` coordinates.
         let uvDest = Vector2(Double(j) + 0.5, Double(i) + 0.5)
 
-        // The position of the destination pixel in the destination image, in coordinates where the
+        // The position of the destination pixel in the source image, in coordinates where the
         // center of the destination image is `(0, 0)`.
         let xyDest = uvDest - 0.5 * Vector2(Double(region.cols), Double(region.rows))
 
-        patch.differentiableUpdate(i, j, to: resample(self, region.center * xyDest))
+        for c in 0..<self.channels {
+          result.update(i, j, c, to: bilinear(self, region.center * xyDest, c))
+        }
       }
     }
-    return patch
+    return result
+  }
+
+  @derivative(of: patch, wrt: region)
+  @usableFromInline
+  func vjpPatch(at region: OrientedBoundingBox) -> (value: Self, pullback: (TangentVector) -> OrientedBoundingBox.TangentVector) {
+    let r = self.patch(at: region)
+    let channels = self.channels
+    func outerPb(_ dOut: TangentVector) -> OrientedBoundingBox.TangentVector {
+      var idx = 0
+      var dBox = OrientedBoundingBox.TangentVector.zero
+      for i in 0..<region.rows {
+        for j in 0..<region.cols {
+          // The position of the destination pixel in the destination image, in `(u, v)` coordinates.
+          let uvDest = Vector2(Double(j) + 0.5, Double(i) + 0.5)
+
+          // The position of the destination pixel in the source image, in coordinates where the
+          // center of the destination image is `(0, 0)`.
+          let xyDest = uvDest - 0.5 * Vector2(Double(region.cols), Double(region.rows))
+
+          for c in 0..<channels {
+            if dOut.pixels.base[idx] != 0 {
+              let pb = pullback(at: region) { bilinear(self, $0.center * xyDest, c) }
+              dBox += pb(dOut.pixels.base[idx])
+            }
+            idx += 1
+          }
+        }
+      }
+      return dBox
+    }
+    return (r, outerPb)
+  }
+}
+
+extension Tensor where Scalar == Double {
+  /// Returns the patch of `self` at `region`.
+  ///
+  /// - Parameters:
+  ///   - self: a pixel tensor with shape `[height, width]` or `[height, width, channelCount]`.
+  ///     `height` and `width` are as defined in `docs/ImageOperations.md`.
+  ///   - region: the center, orientation, and size of the patch to extract.
+  @differentiable(wrt: region)
+  public func patch(at region: OrientedBoundingBox) -> Tensor<Scalar> {
+    precondition(
+      self.shape.count == 2 || self.shape.count == 3,
+      "image must have shape [height, width] or [height, width, channelCount]"
+    )
+    let result = ArrayImage(self).patch(at: region).tensor
+    return self.shape.count == 2 ? result.reshaped(to: [region.rows, region.cols]) : result
   }
 }
 
@@ -57,63 +96,22 @@ extension Tensor where Scalar == Double {
 ///   - image: a pixel tensor, with shape `[height, width, channelCount]`. `height` and
 ///     `width` are as defined in `docs/ImageOperations.md`.
 ///   - point: a point in `(u, v)` coordinates as defined in `docs/ImageOperations.md`.
-@differentiable
-public func bilinear(_ image: Tensor<Double>, _ point: Vector2) -> Tensor<Double> {
-  precondition(image.shape.count == 3)
-
+///   - channel: The channel to return.
+@differentiable(wrt: point)
+public func bilinear(_ image: ArrayImage, _ point: Vector2, _ channel: Int) -> Double {
   // The `(i, j)` integer coordinates of the top left pixel to sample from.
-  let i = withoutDerivative(at: Int(floor(point.y - 0.5)))
-  let j = withoutDerivative(at: Int(floor(point.x - 0.5)))
+  let sourceI = withoutDerivative(at: Int(floor(point.y - 0.5)))
+  let sourceJ = withoutDerivative(at: Int(floor(point.x - 0.5)))
 
-  // Weight of the `(i, _)` pixels in the interpolation.
-  let weightI = Double(i) + 1.5 - point.y
+  // Weight of the `(sourceI, _)` pixels in the interpolation.
+  let weightI = Double(sourceI) + 1.5 - point.y
 
-  // Weight of the `(_, j)` pixels in the interpolation.
-  let weightJ = Double(j) + 1.5 - point.x
+  // Weight of the `(_, sourceJ)` pixels in the interpolation.
+  let weightJ = Double(sourceJ) + 1.5 - point.x
 
-  func pixelOrZero(_ t: Tensor<Double>, _ i: Int, _ j: Int) -> Tensor<Double> {
-    if i < 0 {
-      return Tensor(zeros: [t.shape[2]])
-    }
-    if j < 0 {
-      return Tensor(zeros: [t.shape[2]])
-    }
-    if i >= t.shape[0] {
-      return Tensor(zeros: [t.shape[2]])
-    }
-    if j >= t.shape[1] {
-      return Tensor(zeros: [t.shape[2]])
-    }
-    return t[i, j]
-  }
-
-  let s1 = pixelOrZero(image, i, j) * weightI * weightJ
-  let s2 = pixelOrZero(image, i + 1, j) * (1 - weightI) * weightJ
-  let s3 = pixelOrZero(image, i, j + 1) * weightI * (1 - weightJ)
-  let s4 = pixelOrZero(image, i + 1, j + 1) * (1 - weightI) * (1 - weightJ)
+  let s1 = image[sourceI, sourceJ, channel] * weightI * weightJ
+  let s2 = image[sourceI + 1, sourceJ, channel] * (1 - weightI) * weightJ
+  let s3 = image[sourceI, sourceJ + 1, channel] * weightI * (1 - weightJ)
+  let s4 = image[sourceI + 1, sourceJ + 1, channel] * (1 - weightI) * (1 - weightJ)
   return s1 + s2 + s3 + s4
-}
-
-/// Implements a differentiable update method because the normal subscript setter does not have a
-/// derivative.
-fileprivate extension Tensor where Scalar: TensorFlowFloatingPoint {
-  @differentiable
-  mutating func differentiableUpdate(_ i: Int, _ j: Int, to value: Self) {
-    precondition(self.shape.count == 3)
-    self[i, j] = value
-  }
-
-  @derivative(of: differentiableUpdate)
-  mutating func vjpDifferentiableUpdate(_ i: Int, _ j: Int, to value: Self)
-    -> (value: (), pullback: (inout Self) -> Self)
-  {
-    self.differentiableUpdate(i, j, to: value)
-    let valueShape = value.shape
-    func pullback(_ t: inout Self) -> Self {
-      let tValue = t[i, j]
-      t[i, j] = Tensor(zeros: valueShape)
-      return tValue
-    }
-    return ((), pullback)
-  }
 }
