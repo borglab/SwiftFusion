d7d039c83d69101b606cf1bf788b16d8a18bcb58
Dave Abrahams <dabrahams@google.com>
2020-08-20 17:52:32 -0700

    Add standardBasis tests

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "SwiftFusion",
-      dependencies: ["PenguinStructures"]),
+      dependencies: ["PenguinStructures", "PenguinTesting"]),
     .target(
       name: "SwiftFusionBenchmarks",
       dependencies: [
