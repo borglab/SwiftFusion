a5e30c88726216a84b76a4cd06ba320a129ded11
Fan Jiang <i@fanjiang.me>
2020-09-16 10:02:56 -0400

    Address Frank's comments

--- Sources/SwiftFusion/Inference/PPCAFactor.swift
+++ Sources/SwiftFusion/Inference/PPCAFactor.swift
@@ -15,14 +15,28 @@
 import TensorFlow
 import PenguinStructures
 
-/// A factor that specifies a patch on a latent variable.
+/// A factor that matches a patch on a latent variable.
+/// The decomposition used here is in:
+/// M. E. Tipping and C. M. Bishop, Probabilistic Principal Component Analysis,
+/// Journal of the Royal Statistical Society. Series B, Vol. 61, No. 3 (1999), pp. 611-622
 public struct PPCAFactor: LinearizableFactor1 {
+  /// Type for an image patch with a fixed dimension
   public typealias Patch = Tensor10x10
   public let edges: Variables.Indices
+
+  /// Measured Patch (Template)
   public let measured: Patch
+
+  /// Weight Matrix
   public var W: Tensor<Double>
+  
+  /// Bias
   public var mu: Patch
 
+  /// Constructor
+  /// id: TypedID of the latent vector
+  /// measured: Measured Patch (Template)
+  /// W and mu are calculated by PPCA
   public init(_ id: TypedID<Vector5>, measured: Patch, W: Tensor<Double>, mu: Patch) {
     self.edges = Tuple1(id)
     self.measured = measured
@@ -30,7 +44,11 @@ public struct PPCAFactor: LinearizableFactor1 {
     self.mu = mu
   }
 
+  /// Tangent Vector Type
   public typealias V0 = Vector5
+
+  /// Error Vector for PPCA Factor
+  /// e = W*a + mu - measured
   @differentiable
   public func errorVector(_ a: Vector5) -> Patch.TangentVector {
     return (mu + Patch(matmul(W, a.flatTensor.expandingShape(at: 1)).squeezingShape(at: 2)) - measured)
