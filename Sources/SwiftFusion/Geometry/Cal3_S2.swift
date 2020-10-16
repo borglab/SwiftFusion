7c1e2915fff16038fefb51ff175c49a1a7176d8b
Tuwuh S <tuwuhs@gmail.com>
2020-10-16 15:05:09 -0400

    Add Manifold conformance for Cal3_S2

--- Sources/SwiftFusion/Geometry/Cal3_S2.swift
+++ Sources/SwiftFusion/Geometry/Cal3_S2.swift
@@ -1,6 +1,46 @@
 
 /// The five-parameter camera calibration.
-public struct Cal3_S2: CameraCalibration, Equatable {
+public struct Cal3_S2: CameraCalibration, Manifold, Equatable {
+  public typealias Coordinate = Cal3_S2Coordinate
+  public typealias TangentVector = Vector5
+
+  public var coordinateStorage: Cal3_S2Coordinate
+
+  public init() {
+    self.init(coordinateStorage: Cal3_S2Coordinate())
+  }
+
+  public init(coordinateStorage: Cal3_S2Coordinate) {
+    self.coordinateStorage = coordinateStorage
+  }
+
+  /// Initializes from individual values.
+  public init(fx: Double, fy: Double, s: Double, u0: Double, v0: Double) {
+    self.init(coordinateStorage: Cal3_S2Coordinate(fx: fx, fy: fy, s: s, u0: u0, v0: v0))
+  }
+
+  /// Moves the parameter values by the specified direction.
+  public mutating func move(along direction: Vector5) {
+    coordinateStorage = coordinateStorage.retract(direction)
+  }
+}
+
+/// CameraCalibration protocol conformance.
+extension Cal3_S2 {
+  @differentiable
+  public func uncalibrate(_ np: Vector2) -> Vector2 {
+    coordinate.uncalibrate(np)
+  }
+
+  @differentiable
+  public func calibrate(_ ip: Vector2) -> Vector2 {
+    coordinate.calibrate(ip)
+  }
+}
+
+public struct Cal3_S2Coordinate: ManifoldCoordinate, Equatable {
+  public typealias LocalCoordinate = Vector5
+
   /// Focal length in X direction.
   public var fx: Double
 
@@ -25,14 +65,28 @@ public struct Cal3_S2: CameraCalibration, Equatable {
     self.v0 = v0
   }
 
+  /// Initializes from a vector.
+  public init(_ params: Vector5) {
+    self.fx = params.s0
+    self.fy = params.s1
+    self.s = params.s2
+    self.u0 = params.s3
+    self.v0 = params.s4
+  }
+
   /// Initializes with default values, corresponding to the identity element.
   public init() {
     self.init(fx: 1.0, fy: 1.0, s: 0.0, u0: 0.0, v0: 0.0)
   }
+
+  /// Returns the parameters as a vector.
+  public func asVector() -> Vector5 {
+    Vector5(fx, fy, s, u0, v0)
+  }
 }
 
-/// CameraCalibration protocol conformance.
-extension Cal3_S2 {
+/// Operations on a point.
+extension Cal3_S2Coordinate {
   @differentiable
   public func uncalibrate(_ np: Vector2) -> Vector2 {
     Vector2(
@@ -47,5 +101,18 @@ extension Cal3_S2 {
     return Vector2(
       fxInv * du - s * fxInv * fyInv * dv,
       fyInv * dv)
+  }
+}
+
+/// ManifoldCoordinate conformance.
+extension Cal3_S2Coordinate {
+  @differentiable(wrt: local)
+  public func retract(_ local: Vector5) -> Cal3_S2Coordinate {
+    Cal3_S2Coordinate(asVector() + local)
+  }
+
+  @differentiable(wrt: global)
+  public func localCoordinate(_ global: Cal3_S2Coordinate) -> Vector5 {
+    global.asVector() - asVector()
   }
 }
--- Tests/SwiftFusionTests/Geometry/Cal3_S2Tests.swift
+++ Tests/SwiftFusionTests/Geometry/Cal3_S2Tests.swift
@@ -29,4 +29,18 @@ final class Cal3_S2Tests: XCTestCase {
 
     XCTAssertEqual(K.calibrate(K.uncalibrate(np)), np)
   }
+
+  /// Tests manifold.
+  func testManifold() {
+    var K1 = Cal3_S2(fx: 200.0, fy: 200.0, s: 1.0, u0: 320.0, v0: 240.0)
+    let K2 = Cal3_S2(fx: 201.0, fy: 202.0, s: 4.0, u0: 324.0, v0: 245.0)
+    let dK = Vector5(1.0, 2.0, 3.0, 4.0, 5.0)
+
+    XCTAssertEqual(K1.retract(dK), K2)
+    XCTAssertEqual(K1.localCoordinate(K2), dK)
+
+    K1.move(along: dK)
+    
+    XCTAssertEqual(K1, K2)
+  }
 }
