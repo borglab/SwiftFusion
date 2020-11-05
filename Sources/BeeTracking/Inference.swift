8f29ffcb4b2b894878b0e04ae4b3e1ae585a7962
Marc Rasi <marcrasi@google.com>
2020-11-04 18:49:31 -0800

    add soment documentation and improve inferOneFrameAtATime

--- Sources/BeeTracking/Inference.swift
+++ Sources/BeeTracking/Inference.swift
@@ -2,20 +2,33 @@ import BeeDataset
 import SwiftFusion
 import TensorFlow
 
+/// Settings for infering a track on a video.
 public struct InferenceConfiguration<G: TrackingFactorGraph> {
+  /// The video to track.
   public var video: VOTVideo
 
+  /// The optimizer to use during inference.
   public var optimizer: LM
 
+  /// Given a `frame` and a `region` in the frame, returns the factor graph variables (e.g. pose
+  /// and latent code) corresponding to that frame.
   public typealias MakeFrameVariables =
     (_ frame: Tensor<Double>, _ region: OrientedBoundingBox) -> G.FrameVariables
 
+  /// Creates a factor graph that tracks the object in `frames` that starts out at `start`
+  /// in `frames[0]`.
+  ///
+  /// Parameter guesses: Initial guesses for the variables in all the frames.
   public typealias MakeTrackingFactorGraph =
-    (_ frames: [Tensor<Double>], _ initialization: G.FrameVariables, _ guesses: [G.FrameVariables]) -> G
+    (_ frames: [Tensor<Double>], _ start: G.FrameVariables, _ guesses: [G.FrameVariables]) -> G
 
+  /// See `MakeFrameVariables`.
   public var makeFrameVariables: MakeFrameVariables
+
+  /// See `MakeTrackingFactorGraph`.
   public var makeTrackingFactorGraph: MakeTrackingFactorGraph
 
+  /// Creates a instance with the given settings.
   public init(
     video: VOTVideo,
     optimizer: LM,
@@ -30,13 +43,17 @@ public struct InferenceConfiguration<G: TrackingFactorGraph> {
 }
 
 extension InferenceConfiguration {
+  /// Returns a prediction.
+  ///
+  /// Initialzes a guess that the target stays at the starting point throughout the whole video.
+  /// Then, jointly optimizes all the variables in the guess using LM.
   public mutating func inferAllAtOnce() -> [OrientedBoundingBox] {
     let initialBox = video.track[0]
-    let initialization = makeFrameVariables(video.frames[0], initialBox)
+    let start = makeFrameVariables(video.frames[0], initialBox)
     let tracker = makeTrackingFactorGraph(
       video.frames,
-      initialization,
-      Array(repeating: initialization, count: video.frames.count))
+      start,
+      Array(repeating: start, count: video.frames.count))
     var v = tracker.v
     try? optimizer.optimize(graph: tracker.fg, initial: &v)
     return tracker.poseIds.map {
@@ -44,24 +61,29 @@ extension InferenceConfiguration {
     }
   }
 
+  /// Returns a prediction.
+  ///
+  /// Initializes a guess that the target is at the starting point in frame 1. Uses LM to optimize
+  /// this guess. Takes the solution as a guess for the position in frame 2. Repeats for all
+  /// frames.
   public mutating func inferOneFrameAtATime() -> [OrientedBoundingBox] {
     var predictions = [video.track[0]]
     var frameVariables = [makeFrameVariables(video.frames[0], video.track[0])]
 
-    for i in 0..<(video.frames.count - 1) {
-      let videoSlice = video[i..<(i + 2)]
+    for i in 1..<video.frames.count {
+      let videoSlice = video[i..<(i+1)]
       let tracker = makeTrackingFactorGraph(
         videoSlice.frames,
-        frameVariables[i],
-        Array(repeating: frameVariables[i], count: 2))
+        frameVariables[i - 1],
+        Array(repeating: frameVariables[i - 1], count: 1))
       var v = tracker.v
       try? optimizer.optimize(graph: tracker.fg, initial: &v)
       let prediction = OrientedBoundingBox(
-        center: v[tracker.poseIds[1]],
+        center: v[tracker.poseIds[0]],
         rows: predictions[0].rows,
         cols: predictions[0].cols)
       predictions.append(prediction)
-      frameVariables.append(makeFrameVariables(videoSlice.frames[1], prediction))
+      frameVariables.append(makeFrameVariables(videoSlice.frames[0], prediction))
     }
 
     return predictions
