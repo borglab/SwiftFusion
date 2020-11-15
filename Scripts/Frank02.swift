51d4116529dacc2041153c2ebcd0a09c3f432a65
Fan Jiang <i@fanjiang.me>
2020-11-15 15:10:39 -0500

    Add comments

--- Scripts/Frank02.swift
+++ Scripts/Frank02.swift
@@ -18,15 +18,20 @@ struct Frank02: ParsableCommand {
   var featureSize: Int = 100
 
   // Just runs an RP tracker and saves image to file
+  // Make sure you have a folder `Results/frank02` before running
   func run() {
     let (fig, track, gt) = runRPTracker(
       directory: URL(fileURLWithPath: "./OIST_Data"),
       onTrack: trackId, forFrames: trackLength, withSampling: true,
       withFeatureSize: featureSize
     )
+
+    /// Actual track v.s. ground truth track
     fig.savefig("Results/frank02/frank02_track\(trackId)_\(featureSize).pdf", bbox_inches: "tight")
 
     let testData = OISTBeeVideo(directory: URL(fileURLWithPath: "./OIST_Data"), afterIndex: 100, length: trackLength)!
+
+    /// Plot all the frames so we can visually inspect the situation
     for i in track.indices {
       let (fig_initial, _) = plotPatchWithGT(frame: testData.frames[i], actual: track[i], expected: gt[i])
       fig_initial.savefig("Results/frank02/frank02_1st_img_track\(trackId)_\(featureSize)_\(i).png", bbox_inches: "tight")
