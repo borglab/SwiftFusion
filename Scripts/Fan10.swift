58ff895f2ed5ee45154c48b077788ce44c042375
Fan Jiang <prof.fan@foxmail.com>
2020-11-30 10:54:53 -0500

    Add directory creation logic

--- Scripts/Fan10.swift
+++ Scripts/Fan10.swift
@@ -10,16 +10,16 @@ import Foundation
 /// Fan10: RP Tracker, using the new tracking model
 struct Fan10: ParsableCommand {
   
-  typealias LikelihoodModel = TrackingLikelihoodModel<PretrainedDenseRAE, MultivariateGaussian, GaussianNB>
+  typealias LikelihoodModel = TrackingLikelihoodModel<PretrainedDenseRAE, MultivariateGaussian, MultivariateGaussian>
 
   @Option(help: "Run on track number x")
-  var trackId: Int = 0
+  var trackId: Int = 3
 
   @Option(help: "Run for number of frames")
   var trackLength: Int = 80
 
   @Option(help: "Size of feature space")
-  var featureSize: Int = 30
+  var featureSize: Int = 20
 
   @Flag(help: "Training mode")
   var training: Bool = false
@@ -43,7 +43,6 @@ struct Fan10: ParsableCommand {
   // Make sure you have a folder `Results/fan10` before running
   func run() {
     let kHiddenDimension = 100
-    let np = Python.import("numpy")
     let dataDir = URL(fileURLWithPath: "./OIST_Data")
 
     let generator = ARC4RandomNumberGenerator(seed: 42)
@@ -55,31 +54,43 @@ struct Fan10: ParsableCommand {
     
     let trackingModel = em.run(
       with: trainingData,
-      iterationCount: 5,
+      iterationCount: 3,
+      hook: { i, _, _ in
+        print("EM run iteration \(i)")
+      },
       given: LikelihoodModel.HyperParameters(
         encoder: PretrainedDenseRAE.HyperParameters(hiddenDimension: kHiddenDimension, latentDimension: featureSize, weightFile: "./oist_rae_weight_\(featureSize).npy")
       )
     )
+    
+    let imagesPath = URL(fileURLWithPath: "Results/fan10/fan10_ae_mg_mg")
+    if !FileManager.default.fileExists(atPath: imagesPath.absoluteString) {
+        do {
+            try FileManager.default.createDirectory(atPath: imagesPath.absoluteString, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print(error.localizedDescription);
+        }
+    }
     
     let (fig, track, gt) = runProbabilisticTracker(
       directory: dataDir,
       likelihoodModel: trackingModel,
       onTrack: trackId, forFrames: trackLength, withSampling: true,
       withFeatureSize: featureSize,
-      savePatchesIn: "Results/fan10"
+      savePatchesIn: "Results/fan10/fan10_ae_mg_mg"
     )
 
     /// Actual track v.s. ground truth track
-    fig.savefig("Results/fan10/fan10_track\(trackId)_\(featureSize).pdf", bbox_inches: "tight")
-    fig.savefig("Results/fan10/fan10_track\(trackId)_\(featureSize).png", bbox_inches: "tight")
+    fig.savefig("Results/fan10/fan10_ae_mg_mg_track\(trackId)_\(featureSize).pdf", bbox_inches: "tight")
+    fig.savefig("Results/fan10/fan10_ae_mg_mg_track\(trackId)_\(featureSize).png", bbox_inches: "tight")
 
     let json = JSONEncoder()
     json.outputFormatting = .prettyPrinted
 
     let track_data = try! json.encode(track)
-    try! track_data.write(to: URL(fileURLWithPath: "Results/fan10/fan10_track_\(trackId)_\(featureSize).json"))
+    try! track_data.write(to: URL(fileURLWithPath: "Results/fan10/fan10_ae_mg_mg_track_\(trackId)_\(featureSize).json"))
 
     let gt_data = try! json.encode(gt)
-    try! gt_data.write(to: URL(fileURLWithPath: "Results/fan10/fan10_gt_\(trackId)_\(featureSize).json"))
+    try! gt_data.write(to: URL(fileURLWithPath: "Results/fan10/fan10_ae_mg_mg_gt_\(trackId)_\(featureSize).json"))
   }
 }
