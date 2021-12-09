469e6d1b95c9301faa04bc083fd3f76c4e2f7da8
Andrew Marmon <andrew.marmon@slalom.com>
2021-12-09 11:33:14 -0500

    Update GradientDescentTests.swift

--- Tests/SwiftFusionTests/Optimizers/GradientDescentTests.swift
+++ Tests/SwiftFusionTests/Optimizers/GradientDescentTests.swift
@@ -17,28 +17,28 @@ import SwiftFusion
 import XCTest
 
 final class GradientDescentTests: XCTestCase {
-  /// Test convergence for a simple Pose2SLAM graph.
-  // func testPose2SLAM() {
-  //   var x = VariableAssignments()
-  //   let pose1ID = x.store(Pose2(Rot2(0.2), Vector2(0.5, 0.0)))
-  //   let pose2ID = x.store(Pose2(Rot2(-0.2), Vector2(2.3, 0.1)))
-  //   let pose3ID = x.store(Pose2(Rot2(.pi / 2), Vector2(4.1, 0.1)))
-  //   let pose4ID = x.store(Pose2(Rot2(.pi), Vector2(4.0, 2.0)))
-  //   let pose5ID = x.store(Pose2(Rot2(-.pi / 2), Vector2(2.1, 2.1)))
+  // Test convergence for a simple Pose2SLAM graph.
+  func testPose2SLAM() {
+    var x = VariableAssignments()
+    let pose1ID = x.store(Pose2(Rot2(0.2), Vector2(0.5, 0.0)))
+    let pose2ID = x.store(Pose2(Rot2(-0.2), Vector2(2.3, 0.1)))
+    let pose3ID = x.store(Pose2(Rot2(.pi / 2), Vector2(4.1, 0.1)))
+    let pose4ID = x.store(Pose2(Rot2(.pi), Vector2(4.0, 2.0)))
+    let pose5ID = x.store(Pose2(Rot2(-.pi / 2), Vector2(2.1, 2.1)))
 
-  //   var graph = FactorGraph()
-  //   graph.store(BetweenFactor(pose2ID, pose1ID, Pose2(2.0, 0.0, .pi / 2)))
-  //   graph.store(BetweenFactor(pose3ID, pose2ID, Pose2(2.0, 0.0, .pi / 2)))
-  //   graph.store(BetweenFactor(pose4ID, pose3ID, Pose2(2.0, 0.0, .pi / 2)))
-  //   graph.store(BetweenFactor(pose5ID, pose4ID, Pose2(2.0, 0.0, .pi / 2)))
-  //   graph.store(PriorFactor(pose1ID, Pose2(0, 0, 0)))
+    var graph = FactorGraph()
+    graph.store(BetweenFactor(pose2ID, pose1ID, Pose2(2.0, 0.0, .pi / 2)))
+    graph.store(BetweenFactor(pose3ID, pose2ID, Pose2(2.0, 0.0, .pi / 2)))
+    graph.store(BetweenFactor(pose4ID, pose3ID, Pose2(2.0, 0.0, .pi / 2)))
+    graph.store(BetweenFactor(pose5ID, pose4ID, Pose2(2.0, 0.0, .pi / 2)))
+    graph.store(PriorFactor(pose1ID, Pose2(0, 0, 0)))
 
-  //   let optimizer = GradientDescent(learningRate: 1e-2)
-  //   for _ in 0..<10000 {
-  //     optimizer.update(&x, objective: graph)
-  //   }
+    let optimizer = GradientDescent(learningRate: 1e-2)
+    for _ in 0..<10000 {
+      optimizer.update(&x, objective: graph)
+    }
 
-  //   // Test condition: pose 5 should be identical to pose 1 (close loop).
-  //   XCTAssertEqual(between(x[pose1ID], x[pose5ID]).t.norm, 0.0, accuracy: 1e-2)
-  // }
+    // Test condition: pose 5 should be identical to pose 1 (close loop).
+    XCTAssertEqual(between(x[pose1ID], x[pose5ID]).t.norm, 0.0, accuracy: 1e-2)
+  }
 }
